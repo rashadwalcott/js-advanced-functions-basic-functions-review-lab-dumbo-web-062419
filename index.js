add518305c9a3d1699ce9517940248bc171be7db
Rashad Walcott <rashad.walcott15@gmail.com>
2019-12-03 20:52:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function saturdayFun(activity="roller-skate") {
 
 function mondayWork(activity='go to the office'){
   
-  return (`This Monday, I will ${activity}`)
+  return (`This Monday, I will ${activity}.`)
 }
