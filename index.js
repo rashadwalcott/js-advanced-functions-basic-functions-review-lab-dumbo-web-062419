9a6c86ac3d2149788205ee4c8098a31e1ca16678
Rashad Walcott <rashad.walcott15@gmail.com>
2019-12-03 20:40:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 function saturdayFun(activity='roller-skate') {
   
-  console.log(`This Saturday, I want to ${activity}!`)
+ return console.log(`This Saturday, I want to ${activity}!`)
 }
 
 saturdayFun('bathe my dog!');
